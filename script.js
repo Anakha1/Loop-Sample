585aa13468452cc55b32667426539e01a7e8b637
Anakha1 <129526389+Anakha1@users.noreply.github.com>
2023-04-01 16:01:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+
+//Prints numbers from 0 to 10
+// for(let i =0;i<=10;i++){
+//     console.log(i);
+// }
+
+//Prints numbers from 10 to 0
+
+// for(let i = 10;i>=0;i--){
+//     console.log(i);
+
+// }
+
+//Print the sum of first 10 natural number
+
+// let sum = 0;
+// for(let i = 1; i<=10; i++){
+//     sum = sum + i;
+// }
+// console.log('Sum of first 10 natural numbers is ',sum);
+
+
+//while loop
+
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//do while
+
+let i = 2;
+do{
+    console.log(i);
+    
+}while(i>2);
+
+//for...of loop
+
+let students = ['Hari','Ram','Shine']
+for(let student of students){
+    console.log(`Hello ${student}`);
+
+}
